a0cc66b6a3863c9dbc24f5b646d0ad71a69948e2
Rowen Trombo <rowentrombo@gmail.com>
2023-08-25 00:09:10 -0500

    float decimal 2 final

--- main.py
+++ main.py
@@ -5,38 +5,38 @@ import paygroup
 print('Welcome to RenPay\nPress enter to begin')
 input()
 print('Enter a pay date(mm/dd/yyyy): ')
-pay_date=input()
+pay_date = input()
 #input of employee number
 print('Enter the employee identifier number: ')
-emp_num=int(input())
+emp_num = int(input())
 #input of pay group
 print('Enter the code for pay group the employee is in: ')
-pay_group=input()
+pay_group = input()
 #input of hours
 print('Enter the hours worked: ')
-hours=int(input())
+hours = int(input())
 #input check type
 print('Direct Deposit or Paper Check(D/C): ')
-choice=input()
+choice = input()
 if choice == 'D':
     check_type = 'Direct Deposit'
 else:
     check_type = 'Check'
 #calc hours
-reg_pay = float
-ot_pay=float
-full_pay=float
-if hours<40:
-    reg_pay= 20.00 * hours
+
+if hours < 40:
+    reg_pay = 20.00 * hours
+    ot_pay = 0.00
 
 #calc ot hours
 else:
-    reg_pay= 800.00
-    ot_pay = (hours-40)*30.00
-full_pay=round((reg_pay+ot_pay),2)
+    reg_pay = 800.00
+    ot_pay = float(hours-40) * 30.00
+
+full_pay = reg_pay + ot_pay
 
 #print pay check
-print('Pay Date: '+ pay_date + '                          Employee Number: '+ str(emp_num))
+print('Pay Date: '+pay_date + '                          Employee Number: '+str(emp_num))
 print('Pay Group: '+pay_group+'                             Check Type: '+check_type+'\n')
-print('Hours: '+str(hours)+'.................................Earnings: '+'$'+str(full_pay))
+print('Hours: '+str(hours)+'.................................Earnings: '+'$'+'{:.2f}'.format(full_pay))
 
